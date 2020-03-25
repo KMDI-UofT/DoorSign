f5fa771132fb7ae02c4601a2a0cf2acbb0569967
Mike <andreaem@users.noreply.github.com>
2020-03-25 11:16:10 -0400

    Basic test version of the sign
    This is a very basic ics calender implementation, to be improved over the months and years!

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ This is the website for our door iPad, because we had an older iPad2 we had more
 
 The sign runs on a freemium app called Kiosk Pro Lite which basically loads a webpage without an address bar.  By putting the iPad in Guided Access mode the tablet is effectively in Kiosk mode.
 
+## Improvements needed
+
+This is the most basic version of the door sign.  I am hoping to engage students to improve upon the code.
+
+## References
+
+Thanks to Leonaard for the calendar plugin
+https://github.com/leonaard/icalendar2fullcalendar
+
+This is a combination of FullCalendar and ical.js
---
+++ demo.js
@@ -0,0 +1,49 @@
+ics_sources = [
+    //Cross origin policy error running locally, should work from server
+    {url:'https://outlook.office365.com/owa/calendar/7e6c037200a54bbb89870bdf7153444e@utoronto.ca/0222599322974e3a84fb22a53974a24b6817473586409478242/calendar.ics  ',event_properties:{color:'gold'}},
+    //{url:'samples/events-noend.ics',event_properties:{color:'pink'}},
+    //{url:'samples/32c3.ics'},
+    //{url:'samples/daily_recur.ics',event_properties:{className:['daily-recur'], url:'http://recurring.events.example.org/'}}
+]
+
+
+function data_req (url, callback) {
+    req = new XMLHttpRequest()
+    req.addEventListener('load', callback)
+    req.open('GET', url)
+    req.send()
+}
+
+function add_recur_events() {
+    if (sources_to_load_cnt < 1) {
+        $('#calendar').fullCalendar('addEventSource', expand_recur_events)
+    } else {
+        setTimeout(add_recur_events, 30)
+    }
+}
+
+function load_ics(ics){
+    data_req(ics.url, function(){
+        $('#calendar').fullCalendar('addEventSource', fc_events(this.response, ics.event_properties))
+        sources_to_load_cnt -= 1
+    })
+}
+
+$(document).ready(function() {
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next,today ',
+            center: 'title',
+            right: ''
+        },
+        //basicWeek agendaWeek
+        defaultView: 'basicWeek', //options https://stackoverflow.com/questions/9562475/jquery-full-calendar-default-view-setting
+        defaultDate: new Date(),
+        height: 600,
+    })
+    sources_to_load_cnt = ics_sources.length
+    ics_sources.forEach(function(ics){
+      load_ics(ics)
+    })
+    add_recur_events()
+})
